23c071f9bfc699880230aebccc529926f262eb5c
Tony Trinh <tony@bokksu.com>
2024-07-09 20:19:57 -0400

    added stars temp

--- app/routes/products.$handle.jsx
+++ app/routes/products.$handle.jsx
@@ -9,6 +9,7 @@ import {
   CartForm,
 } from '@shopify/hydrogen';
 import {getVariantUrl} from '~/lib/variants';
+// import { BeakerIcon } from '@heroicons/react/24/solid'
 
 /**
  * @type {MetaFunction<typeof loader>}
@@ -101,6 +102,23 @@ function redirectToFirstVariant({product, request}) {
   );
 }
 
+const emptyStarsIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="size-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+    />
+  </svg>
+);
+
 export default function Product() {
   /** @type {LoaderReturnData} */
   const {product, variants} = useLoaderData();
@@ -148,7 +166,14 @@ function ProductMain({selectedVariant, product, variants}) {
   const {title, descriptionHtml} = product;
   return (
     <div className="product-main">
-      <h1>{title}</h1>
+      <h1 className="text-4xl">{title}</h1>
+
+      {/* <BeakerIcon className="size-6 text-blue-500" /> */}
+      <div className="flex">
+        {[1, 2, 3, 4, 5].map((star) => {
+          return emptyStarsIcon;
+        })}
+      </div>
       <ProductPrice selectedVariant={selectedVariant} />
       <br />
       <Suspense
